2c8a967b5cf726652bdd3bb16618874064008dfa
davseago <davseago@umich.edu>
2019-09-10 23:22:33 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from random import randint
+print('David')
+first = randint(1, 100)
+second = randint(1, 100)
+sum = first + second
+average = (first + second)/2.0
+print(first)
+print(second)
+print("Sum = " + str(sum))
+print("Average = " + str(average))
